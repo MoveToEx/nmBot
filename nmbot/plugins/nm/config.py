d6cedac4d8a8324caf5b4917209a39179b17d313
wqyzstql <1139198820@qq.com>
2024-10-18 10:42:58 +0800

    Add Deepseek Model

--- nmbot/plugins/nm/config.py
+++ nmbot/plugins/nm/config.py
@@ -4,4 +4,5 @@ from pydantic import BaseModel
 class Config(BaseModel):
     nm_gemini_api_key: str
     nm_deepinfra_api_key: str
+    nm_deepseek_api_key: str
     nm_gemini_model: str = 'gemini-1.5-pro-latest'
